458384ee9f553c27a1d7ecaedeabc15b779bff0d
Rafael Reyes <xrafael004@gmail.com>
2024-08-16 10:10:59 -0600

    Pagina login lista

---
+++ CARITAS_frontend/CARITAS_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  CARITAS_frontendApp.swift
+//  CARITAS_frontend
+//
+//  Created by Alumno on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CARITAS_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
