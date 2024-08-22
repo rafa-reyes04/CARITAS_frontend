d9fd6e0fc149a608c50b587a4bcd39bf845cb39c
SantiagoCZB <s.carrizalesb04@gmail.com>
2024-08-21 18:37:20 -0600

    Medio Circulo pantalla perfil

---
+++ CARITAS_frontend/SuperiorPerfilView.swift
@@ -0,0 +1,23 @@
+//
+//  FotoPerfilView.swift
+//  CARITAS_frontend
+//
+//  Created by MacBook Air on 21/08/24.
+//
+
+import SwiftUI
+
+struct FotoPerfilView: View {
+    var body: some View {
+        VStack {
+            Circle()
+                .trim(from: 0.0, to: 0.5)
+                .frame(width: 600)
+                .foregroundColor(Color(red: 0/255, green: 156/255, blue: 166/255))
+        }
+    }
+}
+
+#Preview {
+    FotoPerfilView()
+}
