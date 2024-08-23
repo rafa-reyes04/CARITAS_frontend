81308ad4680fc4b2f2726bad014bf47cdaa474b6
Rafael Reyes <xrafael004@gmail.com>
2024-08-23 09:06:26 -0600

    view perfil

--- CARITAS_frontend/SuperiorPerfilView.swift
+++ CARITAS_frontend/SuperiorPerfilView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct SuperiorPerfilView: View {
     var body: some View {
-        ZStack {
-            Circle()
-                .trim(from: 0.0, to: 0.5)
-                .frame(width: 600)
-                .foregroundColor(Color(red: 0/255, green: 156/255, blue: 166/255))
-        }
+            ZStack {
+                Circle()
+                    .trim(from: 0.0, to: 0.5)
+                    .frame(width: 600)
+                    .foregroundColor(Color(red: 0/255, green: 156/255, blue: 166/255))
+                Text("Nombre")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                    .padding(.top, 280)
+                    
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 180, height: 180)
+                    .padding(.top, 580)
+                    
+                    
+            }
+        
     }
 }
 
